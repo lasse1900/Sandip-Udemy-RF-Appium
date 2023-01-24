ba9df69f2e80cc85524f85fc4193418a1743b671
Lauri Kyttälä <lauri.kyttala@gmail.com>
2023-01-24 13:58:53 +0200

    Multiple_Test_Suite test pass but appium server stays hanging up, not closed as it should be

--- Lesson8/Library/PyLibs/AppiumManager.py
+++ Lesson8/Library/PyLibs/AppiumManager.py
@@ -1,29 +1,42 @@
 import subprocess
 from time import sleep
 
-
+import subprocess
+from time import sleep
+ 
+ 
 class AppiumManager:
     Init_Server_Port = 4723
     Server_Info_Map = {}
-
+ 
     @staticmethod
     def start_appium_server(suite_name, device_id=None):
         curr_port = AppiumManager.Init_Server_Port
-        appium_arg_string = "-p " + str(curr_port) + " -bp " + str(curr_port + 1)
+        appium_arg_string = "-p " + str(curr_port)
         AppiumManager.Init_Server_Port = AppiumManager.Init_Server_Port + 5
         if device_id:
             appium_arg_string = appium_arg_string + " -U " + str(device_id)
-
         print("Starting Appium Server For Suite -" + suite_name + " , Args : " + appium_arg_string)
         appium_server_proc = subprocess.Popen(["appium", appium_arg_string], stdout=subprocess.DEVNULL, shell=True)
         AppiumManager.Server_Info_Map[suite_name] = appium_server_proc
         # NOTE : wait for 5 sec to start the appium server properly #
         sleep(5)
         return curr_port
-
+ 
     @staticmethod
     def kill_appium_server(suite_name):
         print("Stopping Appium Server For Suite - " + str(suite_name))
         appium_server_proc = AppiumManager.Server_Info_Map[suite_name]
         appium_server_proc.terminate()
+        # appium_server_proc.terminate()
+        # sleep(5)
+
+# AppiumManager.start_appium_server("MySuiteOne")
+# AppiumManager.start_appium_server("MySuiteTwo")
+
+# Test execution here
+# sleep(10)
+
+# AppiumManager.kill_appium_server("MySuiteOne")
+# AppiumManager.kill_appium_server("MySuiteTwo")
 
--- Lesson8/Library/Resource/BaseTest.robot
+++ Lesson8/Library/Resource/BaseTest.robot
@@ -2,23 +2,21 @@
 Library     ../PyLibs/AppiumManager.py
 Resource    ../Helpers/AppHelper.robot
 
-
 *** Keywords ***
 BeforeSuite
-    # [Arguments]    ${SUITE_NAME}
-    # ${APPIUM-PORT}    start_appium_server    ${SUITE_NAME}
-    # Open Google Keep Note App    ${APPIUM-PORT}
-    Open Google Keep Note App
-    #Add New Note With Two Items    ${NOTE_TITLE}    ${ITEM-1}    ${ITEM-2}
-    #page should contain text    ${NOTE_TITLE}
+    [Arguments]  ${SUITE_NAME}
+    ${APPIUM-PORT}  start_appium_server  ${SUITE_NAME}
+    Open Google Keep Note App  # ${APPIUM-PORT}
 
 AfterSuite
-    [Arguments]    ${SUITE_NAME}
+    [Arguments]  ${SUITE_NAME}
     Close Google Keep Note App
-    kill_appium_server    ${SUITE_NAME}
+    kill_appium_server  ${SUITE_NAME}
 
 BeforeTest
     Launch Google Keep Note
 
 AfterTest
     Quit Google Keep Note
+
+
